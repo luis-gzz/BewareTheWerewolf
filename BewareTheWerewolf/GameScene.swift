da9edb199133a1cdbbf56ac437a4c2a17c947d0c
Luis Gonzalez <luisgzz@macbook-de-luis.local>
2018-08-16 02:07:21 -0500

    Basic animations setup, added proper iPhoneX images, player movement works properly

--- BewareTheWerewolf/GameScene.swift
+++ BewareTheWerewolf/GameScene.swift
@@ -11,37 +11,62 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
     // Playground
-    var playground: SKSpriteNode?
+    var playground = SKSpriteNode()
     
     // Entity values
-    let playerSpeed: CGFloat = 25.0
+    let playerSpeed: CGFloat = 45.0
     let knightSpeed: CGFloat = 15.0
     
-    var player: SKSpriteNode?
+    var playerShouldMove = true
+    var player =  SKSpriteNode()
+    var wolfAnimationFrames: [SKTexture] = []
+    
     var knights: [SKSpriteNode] = []
     
     var lastTouch: CGPoint? = nil
     
     private var lastUpdateTime : TimeInterval = 0
     
+    override init(size: CGSize) {
+        super.init(size: size)
+        
+        //Setup the playground
+        if (UIScreen.main.nativeBounds.height == 2436) {
+            playground = SKSpriteNode(imageNamed: "playgroundX")
+        } else {
+            playground = SKSpriteNode(imageNamed: "playground")
+        }
+        
+        playground.texture?.filteringMode = SKTextureFilteringMode.nearest
+        playground.position = CGPoint(x: frame.midX, y: frame.midY)
+        playground.zPosition = -1.0
+        addChild(playground)
+        
+        //Set up the player
+        buildPlayerAnimation()
+        player = SKSpriteNode(texture: wolfAnimationFrames[0])
+        player.texture?.filteringMode = SKTextureFilteringMode.nearest
+        player.position.x = frame.midX
+        player.position.y = frame.midY
+        player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 23.0, height: 16.0))
+        player.physicsBody?.affectedByGravity = false
+        player.physicsBody?.allowsRotation = false
+        player.physicsBody?.isDynamic = true
+        
+        addChild(player)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func didMove(to view: SKView) {
         // This func is called when the scene is first presented
         self.lastUpdateTime = 0
         physicsWorld.contactDelegate = self
         
-        //Setup the playground
-        playground = childNode(withName: "playground") as? SKSpriteNode
-        playground?.texture?.filteringMode = SKTextureFilteringMode.nearest
-//        playground?.setScale(6.0)
-        
-        //Set up the player
-        player = childNode(withName: "werewolf") as? SKSpriteNode
-        player?.texture?.filteringMode = SKTextureFilteringMode.nearest
-//        player?.setScale(5.0)
         
         //Setup the knights
         for child in self.children {
@@ -71,6 +96,7 @@ class GameScene: SKScene {
     
     fileprivate func handleTouches(_ touches: Set<UITouch>) {
         lastTouch = touches.first?.location(in: self)
+        playerShouldMove = true;
     }
     
     
@@ -93,27 +119,30 @@ class GameScene: SKScene {
     }
     
     func updatePlayer() {
-        guard let player = player,
-              let touch = lastTouch else {
-                return
-        }
-        
         let position = player.position
         
-        if (shouldPlayerMove(currentPosition: position, touchPosition: touch)) {
-            updatePosition(for: player, to: touch, speed: playerSpeed)
+        if (lastTouch == nil) {
+            return
+        }
+        
+        if (shouldPlayerMove(currentPosition: position, touchPosition: lastTouch!)) {
+            updatePosition(for: player, to: lastTouch!, speed: playerSpeed)
+        } else if (shouldPlayerStop(currentPosition: position, touchPosition: lastTouch!)) {
+            player.physicsBody?.velocity = CGVector(dx: 0.0, dy: 0.0)
+            player.removeAllActions()
+            player.texture = wolfAnimationFrames[0]
         }
         
     }
     
     
     func updateEnemies() {
-        for knight in knights {
-            let targetPosition = player?.position
-            
-            
-            updatePosition(for: knight, to: targetPosition!, speed: knightSpeed)
-        }
+//        for knight in knights {
+//            let targetPosition = player.position
+//
+//
+//            updatePosition(for: knight, to: targetPosition!, speed: knightSpeed)
+//        }
     }
     
     fileprivate func updatePosition(for sprite: SKSpriteNode,
@@ -131,23 +160,56 @@ class GameScene: SKScene {
             sprite.run(flip)
         }
         
+        if sprite.action(forKey: "wolfWalk") == nil {
+            // if legs are not moving, start them
+            let walk = SKAction.animate(with: Array(wolfAnimationFrames[1...4]), timePerFrame: 0.2)
+            sprite.run(SKAction.repeatForever(walk), withKey: "wolfWalk")
+        }
+        
+        
+        
         let velocityX = speed * cos(angle)
         let velocityY = speed * sin(angle)
         
         let newVelocity = CGVector(dx: velocityX, dy: velocityY)
         sprite.physicsBody?.velocity = newVelocity
+        
+        playerShouldMove = false
     }
     
     fileprivate func shouldPlayerMove(currentPosition: CGPoint, touchPosition: CGPoint) -> Bool {
-        guard let player = player else { return false }
-        return abs(currentPosition.x - touchPosition.x) > player.frame.width / 2 ||
-            abs(currentPosition.y - touchPosition.y) > player.frame.height / 2
+        return playerShouldMove && abs(distance(currentPosition, touchPosition)) > player.frame.width / 4
+    }
+    
+    fileprivate func shouldPlayerStop(currentPosition: CGPoint, touchPosition: CGPoint) -> Bool {
+        return abs(distance(currentPosition, touchPosition)) < player.frame.width / 4
+        
     }
     
     // Custom collision handler
     func projectileDidCollideWithEnemy(projectile: SKSpriteNode, enemy: SKSpriteNode) {
         projectile.removeFromParent()
         enemy.removeFromParent()
+    }
+    
+    
+    func buildPlayerAnimation() {
+//        Stand - 0,
+//        Walk1 - 1, Walk2 - 2, Walk3 - 3, Walk4 - 4
+//        Attack1 - 5, Attack2 - 6, Attack3 - 7
+//        Damage1 - 8, Damage2 - 9
+//        Die1 - 10, Die2 - 11, Die3 - 12, Die4 - 13, Die5 - 14
+
+        let wolfAnimatedAtlas = SKTextureAtlas(named: "wolf")
+        var animFrames: [SKTexture] = []
+        
+        let numImages = wolfAnimatedAtlas.textureNames.count
+        for i in 1...numImages {
+            let wolfTextureName = "wolf\(i-1)"
+            
+            animFrames.append(wolfAnimatedAtlas.textureNamed(wolfTextureName))
+        }
+        wolfAnimationFrames = animFrames
     }
     
 }
@@ -178,7 +240,7 @@ extension GameScene: SKPhysicsContactDelegate {
 // === End Physics ===
 
 
-// === Vector Functions ===
+// === Vector+Math Functions ===
 func +(left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
@@ -209,6 +271,12 @@ extension CGPoint {
     func normalized() -> CGPoint {
         return self / length()
     }
+}
+
+func distance(_ a: CGPoint, _ b: CGPoint) -> CGFloat {
+    let xDist = a.x - b.x
+    let yDist = a.y - b.y
+    return CGFloat(sqrt((xDist * xDist) + (yDist * yDist)))
 }
 // === End Vector ===
 
